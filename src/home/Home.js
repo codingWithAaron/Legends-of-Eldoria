97dc7ed88b311f6cd0e23918291bcaaaaab19beb
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-01 18:51:32 -0600

    Created CSS for main title and imported font from Google

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Home.css"
 
 function Home(){
     return (
         <>
-            <div className="container d-flex flex-column text-center mt-4">
+            <div className="container d-flex flex-column text-center mt-4" id="home">
                 <h2>Legends of Eldoria: A Hero's Journey</h2>
                 <p>Story written by ChatGPT</p>
             </div>
