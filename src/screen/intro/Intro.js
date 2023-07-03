44300935877796d4017788eaa8008f8e0b5bc4ae
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-03 10:28:29 -0600

    Uploaded tavern image and added to Intro

--- src/screen/intro/Intro.js
+++ src/screen/intro/Intro.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import image1 from "../accept/images/fotor-ai-20230703102627.jpg"
 
 function Intro(){
  return (
     <>
         <div className="container d-flex flex-column m-5">
+            <div className="d-flex justify-content-center">
+                <img className="w-50 mb-4" src={image1} alt="hidden village"/>
+            </div>
             <div className="text-center">
                 <h3>Once upon a time, in the mystical land of Eldoria, you find yourself sitting in the bustling tavern of Thornwood. The aroma of hearty stew and the sound of merry laughter fill the air. As a weary traveler, you seek solace and rest, but fate has something else in store for you.</h3>
             </div>
