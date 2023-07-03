8b9673edc9c1dfa2614bcad6361f6652a9e87e26
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-03 10:11:01 -0600

    Added images to Mountains component

--- src/screen/accept/Mountains.js
+++ src/screen/accept/Mountains.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import image1 from "../accept/images/fotor-ai-2023070375729.jpg"
+import image2 from "../accept/images/fotor-ai-2023070375723.jpg"
 
 function Mountains(){
  return (
     <>
         <div className="container d-flex flex-column m-5">
+            <div className="d-flex">
+                <div className="me-4">
+                    <img className="w-100" src={image1} alt="treacherous peaks"/>
+                </div>
+                <div>
+                    <img className="w-100" src={image2} alt="winding valley"/>
+                </div>
+            </div>
             <div className="text-center">
                 <div className="mb-5">
                     <h4>Eventually, you arrive at the base of the treacherous mountains. The weather takes a dramatic turn. Dark clouds gather overhead, obscuring the sunlight and casting a gloomy shadow over the landscape. The wind howls fiercely, whistling through the jagged peaks and sending a chill down your spine. Snow begins to fall, lightly at first but gradually increasing in intensity. Here, you face another crucial decision:</h4>
