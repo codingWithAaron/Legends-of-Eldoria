4e0b9c66181f49022158e6eacac4ca52853a8478
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-03 10:35:10 -0600

    Uploaded town image and added to Start

--- src/screen/start/Start.js
+++ src/screen/start/Start.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import image1 from "../accept/images/fotor-ai-20230703103355.jpg"
 
 function Start(){
  return (
     <>
         <div className="container d-flex flex-column m-5">
+            <div className="d-flex justify-content-center">
+                <img className="w-50 mb-4" src={image1} alt="town of thornwood"/>
+            </div>
             <div className="text-center">
                 <h2>Welcome Adventurer! You're journey starts here!</h2>
             </div>
