607eec5e4204d35f8abacc4df1841be2efb2135a
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-03 10:24:58 -0600

    Uploaded new map image and added to AcceptIntro

--- src/screen/accept/AcceptIntro.js
+++ src/screen/accept/AcceptIntro.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import image1 from "../accept/images/fotor-ai-20230703102225.jpg"
 
 function AcceptIntro(){
  return (
     <>
         <div className="container d-flex flex-column m-5">
+            <div className="d-flex justify-content-center">
+                <img className="w-50 mb-4" src={image1} alt="map"/>
+            </div>
             <div className="text-center">
                 <div className="mb-5">
                     <h4>The elder's eyes gleam with hope as he provides you with a map to the dragon's lair. The map reveals that the journey will take you through treacherous forests, treks across perilous mountains, and a labyrinthine cave system before reaching the dragon's abode. Armed with the knowledge of the dragon's location, you embark on your epic quest.</h4>
