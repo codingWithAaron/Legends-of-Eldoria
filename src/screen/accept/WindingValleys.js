0994fefa3248e0e1fa6b798b85898b872653d822
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-03 10:17:43 -0600

    Added image to WindingValleys

--- src/screen/accept/WindingValleys.js
+++ src/screen/accept/WindingValleys.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import image2 from "../accept/images/fotor-ai-2023070375946.jpg"
 
 function WindingValleys(){
  return (
     <>
         <div className="container d-flex flex-column m-5">
+            <div className="d-flex justify-content-center">
+                <img className="w-50 mb-4" src={image2} alt="hidden village"/>
+            </div>
             <div className="text-center">
                 <div className="mb-5">
                     <h4>Feeling at ease for taking a safer route, you eventually come across a hidden village nestled within the valleys. Grateful for your presence, the villagers offer you a magical potion that grants temporary invulnerability against fire. With this valuable asset, you continue your journey with renewed determination, knowing that the dragon's flames won't harm you.</h4>
