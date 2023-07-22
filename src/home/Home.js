1858c5a80597edbe40f6e83aa34c86eb3d53c6c7
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-21 18:46:45 -0600

    removed "Story written by chatGPT"

--- src/home/Home.js
+++ src/home/Home.js
@@ -6,7 +6,6 @@ function Home(){
         <>
             <div className="container d-flex flex-column text-center mt-4" id="home">
                 <h2>Legends of Eldoria: A Hero's Journey</h2>
-                <p>Story written by ChatGPT</p>
             </div>
         </>
     )
