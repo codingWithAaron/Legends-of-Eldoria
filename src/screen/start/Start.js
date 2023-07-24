aa0b368f154baa55871ecaacb51841aee5f91754
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-07-24 14:10:03 -0600

    Spelling error fixed

--- src/screen/start/Start.js
+++ src/screen/start/Start.js
@@ -7,13 +7,13 @@ function Start(){
     <>
         <div className="container d-flex flex-column m-5">
             <div>
-                <h4 className=" text-center d-flex justify-content-center mb-4 border-bottom border-3 border-dark p-2">Legends of Eldoria: A Hero's Journey</h4>
+                <h4 className=" text-center d-flex justify-content-center mb-4 border-bottom border-3 border-dark p-2 typed container">Legends of Eldoria: A Hero's Journey</h4>
             </div>
             <div className="d-flex justify-content-center">
                 <img className="w-25 mb-4" src={image1} alt="town of thornwood"/>
             </div>
-            <div className="text-center mt-3">
-                <h5>Welcome Adventurer! You're journey starts here!</h5>
+            <div className="text-center mt-3 typed container">
+                <h5>Welcome Adventurer! Your journey starts here!</h5>
             </div>
             <div className="text-center mt-5">
                 <Link to="/intro" className="btn btn-danger">Start</Link>
